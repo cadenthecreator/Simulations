9f6f322279820b712871e808b368e7a8844aa6c3
squidoctopus360 <hudsonream@gmail.com>
2025-04-02 10:10:15 -0700

    get and setblock functions

--- src/main/java/org/caden/simulations/Peripherals/SimulatorPeripheral.java
+++ src/main/java/org/caden/simulations/Peripherals/SimulatorPeripheral.java
@@ -1,13 +1,26 @@
 package org.caden.simulations.Peripherals;
 
 import dan200.computercraft.api.lua.LuaFunction;
+import dan200.computercraft.api.lua.MethodResult;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.Registries;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
 import org.caden.simulations.Entities.SimulatorEntity;
+import org.checkerframework.checker.nullness.qual.RequiresNonNull;
+import org.jetbrains.annotations.NotNull;
 import org.jspecify.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 public class SimulatorPeripheral implements IPeripheral {
 
@@ -27,9 +40,45 @@ public class SimulatorPeripheral implements IPeripheral {
         return other instanceof SimulatorPeripheral o && simulator == o.simulator;
     }
 
+    private Map<String, Object> serializeBlockState(@NotNull BlockState state) {
+        Map<String, Object> out = new HashMap<>();
+        out.put("name",state.getBlock().getName());
+        out.put("id",Registries.BLOCK.getId(state.getBlock()).toString());
+        for (Property<?> property : state.getProperties()) {
+            Comparable<?> value = state.get(property);
+
+            // Add value based on type
+            if (value instanceof Boolean) {
+                out.put(property.getName(), (Boolean) value);
+            } else if (value instanceof Integer) {
+                out.put(property.getName(), (Integer) value);
+            } else if (value instanceof Float) {
+                out.put(property.getName(), (Float) value);
+            } else if (value instanceof Double) {
+                out.put(property.getName(), (Double) value);
+            } else {
+                out.put(property.getName(), value.toString()); // fallback for enums like Direction
+            }
+        }
+        return out;
+    }
+
     @LuaFunction
-    public void test() {
-        Objects.requireNonNull(this.simulator.getWorld()).setBlockState(this.simulator.getPos(),Blocks.AIR.getDefaultState());
+    public final MethodResult getBlock(int x,int y,int z) {
+        BlockState blockState = simulator.worldHandle.asWorld().getBlockState(new BlockPos(x,y,z));
+        return MethodResult.of(serializeBlockState(blockState));
+    }
+
+    @LuaFunction
+    public final MethodResult setBlock(int x,int y,int z, String block) {
+        if (Identifier.tryParse(block) != null) {
+            Optional<Block> newblock = Registries.BLOCK.getOrEmpty(Identifier.tryParse(block));
+            if (newblock.isPresent()) {
+                simulator.worldHandle.asWorld().setBlockState(new BlockPos(x, y, z), newblock.get().getDefaultState());
+                return MethodResult.of(true);
+            }
+        }
+        return MethodResult.of(false);
     }
 
     @Override
